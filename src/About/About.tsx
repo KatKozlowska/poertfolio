0c0b7e0f1c878b9055243e9ef61399737696e331
Katarzyna Kozlowska <Kat.w.kozlowska@gmail.com>
2024-01-16 09:45:45 +0000

    updated fonts and styles

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -4,15 +4,15 @@ import me from "../assets/images/author.jpg";
 const About = () => {
   return (
     <section className="about" id="about">
-    <h1>ABOUT ME</h1>
+    <h1 className="about__heading">ABOUT ME</h1>
    <div className="about__div">
       <p className="about__p">
         Hi! My name is Katarzyna (Kat) and am a hardworking, proactive, and
         creative individual with experience in people management. Previously I
         was a Designer and a Showroom manager which has helped me develop strong
         project management, leadership, and teamwork skills. I have always
-        loved creating, which made me peruse architecture; which allowed to to
-        (develop)critical thinking, public speaking, etc?? After encountering
+        loved creating, which made me peruse a degree in architecture; which allowed me to sharpen up my
+        critical thinking, public speaking and attention to detail. After encountering
         software development through my peers, I could not help but think of
         the endless possibilities when you are not limited to physical
         materials. Under the guidance of _nology I am keen to bring a creative
--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
             <div className="nav__buttons">
             <a href="#about" className="nav__button nav__button--about" >About</a>
             <a href="#projects" className="nav__button nav__button--portfolio">Projects</a>
-            <a href="#contact" className="nav__button nav__button--contact">Contact</a>
+            <a href="#footer" className="nav__button nav__button--contact">Contact</a>
             </div>
         </nav>
 
